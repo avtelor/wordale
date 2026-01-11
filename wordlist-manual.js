5be5f847ce09957901b5a339a5e061579cf729be
Avi Tel-Or <avi.telor@gmail.com>
2026-01-12 00:04:52 +0200

    Update wordlist in manual mode by replacing existing entries with new words for enhanced gameplay experience.

--- wordlist-manual.js
+++ wordlist-manual.js
@@ -1,7 +1,7 @@
 // Assign to global scope (without let/const) to overwrite the main wordlist temporarily
 listOfWords = [
-"שמיכה",
-"שבלול",
-"שמירה",
-"שבתאי",
+"כתומה",
+"נסיעה",
+"משימה",
+"הודיה",
 ];
